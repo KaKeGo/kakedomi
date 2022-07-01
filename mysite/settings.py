42b1c4002fb6fd674e8dcc4eb9f296c9a04411a8
KaKeGo <funfcia@hotmail.com>
2022-07-01 20:17:50 +0200

    add html: master, navbar, footer, and in home app home.html add app to settings add urls to home app

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #My apps
+    'home',
+
+    #Third apps
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
