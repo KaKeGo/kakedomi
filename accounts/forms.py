d0cb6fa632fd4fef1e67d201ddd6c49a749890b2
KaKeGo <funfcia@hotmail.com>
2022-07-01 20:47:40 +0200

    add forms.py create user creation form create user change form create user create form

---
+++ accounts/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from .models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise ValidationError('Password don`t match')
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=True)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean_password(self):
+        return self.initial['password']
+
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.username = self.cleaned_data['username']
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
