00fe64bf603039e9dcf1c53fef4108c40b1a9d38
KaKeGo <funfcia@hotmail.com>
2022-07-01 21:28:43 +0200

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yu@rqoizf3+!k%v#4y)3tz)ug7v6*vvn)l&rj*v*iogd#+u+pm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://kakedomi.herokuapp.com']
 
 
 # Application definition
