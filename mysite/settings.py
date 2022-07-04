8734a0a9ca23f1e0a525a59d3250a16b302d2352
KaKeGo <funfcia@hotmail.com>
2022-07-04 21:34:08 +0200

    install django-core-headers and add to middleware and apps

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,11 +43,14 @@ INSTALLED_APPS = [
     'accounts',
 
     #Third apps
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
