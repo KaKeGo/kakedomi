0bd683fd239dec79a0bc185f19df5ea8f51d40d1
KaKeGo <funfcia@hotmail.com>
2022-07-09 11:26:01 +0200

    add serializers for profile create first profile api view add url for profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import (
+    Profile,
+)
+from .serializers import (
+    ProfileSerializer,
+)
 # Create your views here.
+
+@api_view(['GET'])
+def profile_view(request, slug):
+    qs = Profile.objects.filter(slug=slug)
+    if not qs.exists():
+        return Response({}, status=404)
+    obj = qs.first()
+    serializer = ProfileSerializer(obj)
+    return Response(serializer.data)
--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yu@rqoizf3+!k%v#4y)3tz)ug7v6*vvn)l&rj*v*iogd#+u+pm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000']
 
