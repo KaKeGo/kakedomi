9759a72242c72c6600f1de0b8dd44f83c263770f
KaKeGo <funfcia@hotmail.com>
2022-07-09 14:57:11 +0200

    create todo model and add to admin

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.shortcuts import reverse
 
 # Create your models here.
+
+
+class ToDo(models.Model):
+    body = models.CharField(max_length=155)
+    completed = models.BooleanField(default=False)
+    create_on = models.DateTimeField(auto_now_add=True)
+    update_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.body}'
+
+    class Meta:
+        ordering = ['-create_on']
+        verbose_name_plural = 'ToDo'
+        verbose_name = 'ToDo'
