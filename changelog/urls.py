ff6e0eb17163e6459466f6f0f69cedb8c1cb1742
KaKeGo <funfcia@hotmail.com>
2022-07-10 17:01:30 +0200

    Create view for delete and create Add path to views

--- changelog/urls.py
+++ changelog/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 
 from .views import (
     changelog_list_view,
+    changelog_create_view,
+    changelog_delete_view,
 )
 
 app_name = 'changelog'
 
 urlpatterns = [
     path('', changelog_list_view, name='changelog'),
+    path('create/', changelog_create_view, name='create'),
+    path('<str:changelog_id>/delete/', changelog_delete_view, name='delete'),
 ]
