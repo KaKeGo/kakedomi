54b258fbaafd72ca342b0f9aac4e5dcc4e7ae90e
KaKeGo <funfcia@hotmail.com>
2022-07-16 11:18:41 +0200

    Create serializers for Contacts models

---
+++ contact/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from .models import Contact, ContactMessage
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = ['title', 'body']
+
+class ContactMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactMessage
+        fields = ['title', 'body', 'postman', 'slug', 'create_on', 'update_on', 'accepted']
