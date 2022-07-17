5241d2091d33721075ae21ca20554b11767a9734
KaKeGo <funfcia@hotmail.com>
2022-07-17 21:55:18 +0200

    change CORS_ALLOWED_ORIGINS

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,9 +131,10 @@ USE_TZ = True
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "http://kakedomi-backend.herokuapp.com",
+    "http://kakedomi.herokuapp.com",
 ]
 
 DEFAULT_RENDERER_CLASSES = [
